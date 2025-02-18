d2e10083955f986d1e02adb18b31ac8a9b807770
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-02-18 21:24:40 +0530

    Time: 1264 ms (83%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+WITH dummy AS(
+    SELECT d.name AS Department,e.name as Employee,e.salary as Salary,
+DENSE_RANK() OVER(PARTITION BY d.id ORDER BY e.salary DESC) AS RANK
+FROM employee e JOIN  Department d ON e.departmentId=d.id
+)
+
+SELECT Department,Employee,Salary
+FROM dummy WHERE rank<4;
