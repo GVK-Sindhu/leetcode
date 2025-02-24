b2afaf2f0f4cd0eebc7c01dc2b7ca8ced182d944
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-02-24 11:14:00 +0530

    Time: 3652 ms (5.21%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,10 +1,16 @@
 /* Write your PL/SQL query statement below */
 
+-- WITH cte AS(
+-- SELECT e.id,e.name AS Employee,e.salary AS Salary,e.departmentId,d.name AS Department,
+-- MAX(e.salary) OVER(PARTITION BY d.name)AS mx_salary
+-- FROM Employee e JOIN Department d
+-- ON e.departmentId=d.id
+-- WHERE e.salary IS NOT NULL
+-- )
+-- SELECT Department,Employee,Salary FROM cte WHERE salary=mx_salary;
 WITH cte AS(
-SELECT e.id,e.name AS Employee,e.salary AS Salary,e.departmentId,d.name AS Department,
-MAX(e.salary) OVER(PARTITION BY d.name)AS mx_salary
+SELECT e.name AS Employee,e.salary AS Salary,d.name AS Department,
+RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC)AS rank
 FROM Employee e JOIN Department d
-ON e.departmentId=d.id
-WHERE e.salary IS NOT NULL
-)
-SELECT Department,Employee,Salary FROM cte WHERE salary=mx_salary;
+ON e.departmentId=d.id)
+SELECT Department,Employee,Salary FROM cte WHERE rank=1;
