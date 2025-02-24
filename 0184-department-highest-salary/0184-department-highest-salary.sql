1dcd19d86305b28e0f625fcd033f33d0e7b3a28a
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-02-24 10:55:34 +0530

    Time: 1479 ms (35%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+
+WITH cte AS(
+SELECT e.id,e.name AS Employee,e.salary AS Salary,e.departmentId,d.name AS Department,
+MAX(e.salary) OVER(PARTITION BY d.name)AS mx_salary
+FROM Employee e JOIN Department d
+ON e.departmentId=d.id
+WHERE e.salary IS NOT NULL
+)
+SELECT Department,Employee,Salary FROM cte WHERE salary=mx_salary;
