cfb7dbdfda1444a5b2172468727843750c83e22a
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-02-27 17:17:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int reverse(int x) {
-        int temp=x;
-        int rev=0;
-        while(temp!=0){
-            int ld=temp%10;
-            temp/=10;
-            if(rev>INT_MAX/10 || (rev==INT_MIN/10 && ld>7)){
-                return 0;
-            }
-            if(rev<INT_MIN/10||(rev==INT_MIN/10 && ld<-8)){
-                return 0;
-            }
-            rev=rev*10+ld;
+       int rev=0;
+       while(x!=0){
+        int ld=x%10;
+        x=x/10;
+        if(rev>INT_MAX/10 || (rev=INT_MAX/10 && ld>7)){
+            return 0;
         }
-        return rev;
+        if(rev<INT_MIN/10 ||(rev=INT_MIN/10 && ld<-8)){
+            return 0;
+        }
+        rev=rev*10+ld;
+       }
+       return rev;
     }
 };
