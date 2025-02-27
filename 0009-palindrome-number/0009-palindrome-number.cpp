61877a30582a3d5c8ec91df09fef765311c07989
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-02-27 17:31:22 +0530

    Time: 4 ms (33.75%), Space: 8.4 MB (92.32%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long rev=0;
-        long long temp=x;
-        while(x!=0){
-            int ld=x%10;
-            rev=rev*10+ld;
+        if(x<0||(x%10==0 && x!=0)) return false;
+        int rev=0;
+        int temp=x;
+        while(x>rev){
+            rev=rev*10+x%10;
             x/=10;
         }
-        if(rev==temp && temp>=0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return x==rev||x==rev/10;
     }
 };
