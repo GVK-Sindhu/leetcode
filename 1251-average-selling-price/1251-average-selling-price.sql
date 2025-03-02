f3f58ed6d77de193cabed9ff33aa235675733739
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-03-02 21:25:21 +0530

    Time: 1426 ms (53.87%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-SELECT  p.product_id,IFNULL(ROUND(SUM(u.units*p.price)/SUM(u.units),2),0) AS average_price
-FROM Prices p JOIN UnitsSold u ON p.product_id=u.product_id and u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id
+/* Write your PL/SQL query statement below */
+SELECT p.product_id as product_id,NVL(ROUND(SUM(p.price*u.units)/SUM(u.units),2),0) as average_price
+FROM Prices p LEFT JOIN UnitsSold u ON p.product_id=u.product_id and u.purchase_date BETWEEN p.start_date and p.end_date
+GROUP BY p.product_id;
