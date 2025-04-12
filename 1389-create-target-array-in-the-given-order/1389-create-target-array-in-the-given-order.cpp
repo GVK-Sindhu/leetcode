2685033a7250821e120a6d742f18d879a51f3bfd
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-04-12 21:49:12 +0530

    Time: 0 ms (100%), Space: 11.2 MB (90.98%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-        int n=nums.size();
         vector<int>res;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             res.insert(res.begin()+index[i],nums[i]);
         }
         return res;
