dc8c1c2bd1da4f54a3023c03247df8827232ec7d
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-05-07 19:31:40 +0530

    Time: 0 ms (100%), Space: 11.6 MB (15.82%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        vector<string>code={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        unordered_set<string>transformations;
-        for(string it:words){
-            string i="";
-            for(char ch:it){
-                i+=code[ch-'a'];//to iterate over all the letters of word and append them into i  i=".-"+code['c'-'a']
+        // vector<string>code={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        // unordered_set<string>transformations;
+        // for(string it:words){
+        //     string i="";
+        //     for(char ch:it){
+        //         i+=code[ch-'a'];//to iterate over all the letters of word and
+        //         append them into i  i=".-"+code['c'-'a']
+        //     }
+        //     transformations.insert(i);
+        // }
+        // return transformations.size();
+        unordered_map<char,string> code={
+        {'a', ".-"}, {'b', "-..."}, {'c', "-.-."}, {'d', "-.."}, {'e', "."}, {'f', "..-."}, {'g', "--."}, {'h', "...."}, {'i', ".."}, {'j', ".---"}, {'k', "-.-"}, {'l', ".-.."}, {'m', "--"}, {'n', "-."}, {'o', "---"}, {'p', ".--."}, {'q', "--.-"}, {'r', ".-."}, {'s', "..."}, {'t', "-"}, {'u', "..-"}, {'v', "...-"}, {'w', ".--"}, {'x', "-..-"}, {'y', "-.--"}, {'z', "--.."}, 
+        };
+        unordered_set<string> ans;
+        for (string it : words) {
+            string temp = "";
+            for (char ch : it) {
+                temp += code[ch];
             }
-            transformations.insert(i);
+            ans.insert(temp);
         }
-        return transformations.size();
+        return ans.size();
     }
 };
