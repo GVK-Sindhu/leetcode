db0f3b093f52d933c6201a5a018ff506d8e7eeb0
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-05-22 20:57:32 +0530

    Time: 13 ms (40.29%), Space: 32.1 MB (55.13%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<int>res;
-        for(int i=0;i<nums.size()-1;i+=2){
-            int ans=min(nums[i],nums[i+1]);
-            res.push_back(ans);
-        }
         int s=0;
-        for(int it:res){
-            s+=it;
+        for(int i=0;i<nums.size()-1;i+=2){
+            s+=nums[i];
         }
+        
         return s;
     }
 };
