5bf55ac7a09828802079149c92d1fdde9c0a7995
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-05-23 16:39:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int c=0,c1=0,c2=0;
-        for(auto it:nums){
-            if(it==0){
-                c++;
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int tmp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=tmp;
+                mid++;
+                low++;
             }
-            else if(it==1){
-                c1++;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                c2++;
+                int t=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=t;
+                mid++;
+                high--;
             }
-        }
-        for(int i=0;i<c;i++){
-            nums[i]=0;
-        }
-        for(int i=c;i<(c+c1);i++){
-            nums[i]=1;
-        }
-        for(int i=(c+c1);i<(c+c1+c2);i++){
-            nums[i]=2;
         }
     }
 };
