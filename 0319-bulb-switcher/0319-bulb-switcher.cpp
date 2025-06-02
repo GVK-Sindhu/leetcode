5d9c3ed902b57b6af5cb7f1908602232bee6ad2b
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-06-02 21:50:10 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.89%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // if(n==0){
+        //     return 0;
+        // }
+        // if(n==1){
+        //     return 3;
+        // }
+        return int(sqrt(n));
+
+        
+    }
+};
