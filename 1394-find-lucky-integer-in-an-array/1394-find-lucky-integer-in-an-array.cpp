b6e995a73bfff13bf45ee98209e72f0711dd9f6c
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-07-05 21:33:53 +0530

    Time: 0 ms (100%), Space: 13.9 MB (65.75%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int findLucky(vector<int>& arr) {
-        unordered_map<int,int>freq;
+        map<int,int>freq;
         for(int i=0;i<arr.size();i++){
             freq[arr[i]]++;
         }
         int maxl=-1;
         for(auto it:freq){
             if(it.first==it.second){
-                if(it.first>maxl){
                     maxl=it.first;
-                }
             }
         }
         return maxl;
