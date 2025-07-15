27858a3d602cf10a8a6bec143b3ae57963eec28f
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-07-15 20:17:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        int vow=0,con=0;
+        if(word.length()>=3){
+        for(char c:word){
+            if(!isalnum(c)){
+                return false;
+            }
+            if(isalnum(c)){
+                if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
+                    vow++;
+                }
+                else{
+                    con++;
+                }
+            }
+        }
+        if(vow>0 && con>0){
+           
+            return true;
+        }
+        }
+        cout<<vow<<" "<<con<<endl;
+
+        return false;
+    }
+};
