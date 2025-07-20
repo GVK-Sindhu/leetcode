70f6c32347a2c45eb7f5ec3c4422b13d8673e022
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-07-20 21:17:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3457-eat-pizzas/3457-eat-pizzas.cpp
+++ 3457-eat-pizzas/3457-eat-pizzas.cpp
@@ -6,14 +6,24 @@ public:
         int n=pizzas.size();
         int i=n-1;
         int grp=n/4;
-        for(int day=1;day<=grp;day+=2){
-                total+=pizzas[i--];
-        }
-        i--;
-        for(int day=2;day<=grp;day+=2){
+        for(int day=1;day<=grp;day++){
+            if(day%2==1){
                 total+=pizzas[i];
+            }
             i-=2;
-        }   
+            else{
+                total+=pizzas[i];
+            }
+            i-=2;
+        }
+        // for(int day=1;day<=grp;day+=2){
+        //         total+=pizzas[i--];
+        // }
+        // i--;
+        // for(int day=2;day<=grp;day+=2){
+        //         total+=pizzas[i];
+        //     i-=2;
+        // }   
         return total;                                                                   
 
     }
