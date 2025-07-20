492360fc57b369cabdb986160f9cb0c032c6642a
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-07-20 21:05:24 +0530

    Time: 82 ms (48.84%), Space: 180.1 MB (59.64%) - LeetHub

---
+++ 3457-eat-pizzas/3457-eat-pizzas.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxWeight(vector<int>& pizzas) {
+        sort(pizzas.begin(),pizzas.end());
+        long long total=0;
+        int n=pizzas.size();
+        int i=n-1;
+        int grp=n/4;
+        for(int day=1;day<=grp;day+=2){
+                total+=pizzas[i--];
+        }
+        i--;
+        for(int day=2;day<=grp;day+=2){
+                total+=pizzas[i];
+            i-=2;
+        }   
+        return total;                                                                   
+
+    }
+};
