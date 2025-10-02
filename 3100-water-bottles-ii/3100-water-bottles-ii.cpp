1fa4a39d890183075e1c3ef05d26e85bdaa55206
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-10-02 14:45:39 +0530

    Time: 0 ms (100%), Space: 8.4 MB (94.63%) - LeetHub

---
+++ 3100-water-bottles-ii/3100-water-bottles-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int res=numBottles;
+        while(numBottles>=numExchange){
+            res++;
+            numBottles=numBottles-(numExchange-1);
+            numExchange++;
+        }
+        return res;
+    }
+};
+
