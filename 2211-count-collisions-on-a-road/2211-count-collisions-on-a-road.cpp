d39cc5840dbf504072ad9980dcdef762795fee64
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-12-04 15:54:06 +0530

    Time: 8 ms (78.26%), Space: 19.1 MB (84.64%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -3,17 +3,15 @@ public:
     int countCollisions(string directions) {
         int l=0,r=directions.size()-1;
         int lc=0,rc=0;
-        while(l<r && directions[l]=='L'){
+        while(l<directions.size() && directions[l]=='L'){
             lc++;
             l++;
         }
-        while(r>0 && directions[r]=='R'){
+        while(r>=0 && directions[r]=='R'){
             rc++;
             r--;
         }
         int c=0;
-        int total=directions.size();
-        int torem=total-(rc+lc);
         for(int i=l;i<=r;i++){
             if(directions[i]=='S'){
                 continue;
