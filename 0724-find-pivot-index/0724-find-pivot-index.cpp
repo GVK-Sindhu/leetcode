d6a3b935ab1de89f05d1b8110d2c2b036e218fae
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-12-22 20:51:31 +0530

    Time: 0 ms (100%), Space: 36.4 MB (20.18%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int ts=0;
-        for(int it:nums){
-            ts+=it;
+        int n=nums.size();
+        vector<int>ps(n);
+        ps[0]=nums[0];
+        for(int i=1;i<n;i++){
+            ps[i]=ps[i-1]+nums[i];
         }
-        int ls=0;
-        for(int i=0;i<nums.size();i++){
-            int rs=ts-ls-nums[i];
-            if(rs==ls){
-                return i;
-            }  
-            ls+=nums[i];
+        int res=-1,ls=0,rs=0;
+
+        for(int i=0;i<n;i++){
+            if(i==0 ){
+                ls=0;
+            }
+            else{
+                ls=ps[i-1];
+            }
+            if(i==n-1){
+                rs=0;
+            }
+            else{
+                rs=ps[n-1]-ps[i];
+            }
+            if(ls==rs){
+                res=i;
+                break;
+            }
         }
-        return -1;
+        return res;
     }
 };
