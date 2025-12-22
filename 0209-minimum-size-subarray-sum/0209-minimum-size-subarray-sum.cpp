c8f6ea7c0bb892c7b1e0fe9df7e9fe4b2e1b1dc0
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2025-12-22 15:47:59 +0530

    Time: 55 ms (5.24%), Space: 32.1 MB (38.19%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int i=0,j=0;
-        int minl=INT_MAX;
         int n=nums.size();
-        int s=0;
-        while(j<n){
-            s+=nums[j];
+        int minlen=INT_MAX,len=0;
+        int l=0,r=0,s=0;
+        while(r<n){
+            s+=nums[r];
             while(s>=target){
-                minl=min(minl,(j-i+1));
-                s-=nums[i];
-                i++;
+            len=(r-l+1);
+            cout<<len<<s<<" ";
+            minlen=min(minlen,len);
+                s-=nums[l];
+                l++;  
             }
-            j++;
+            r++;
         }
-        return minl==INT_MAX?0:minl;
+        return minlen==INT_MAX?0:minlen;
     }
 };
