5d11a0db407183ab00a13f5fd3b5d5fdfb1df4cf
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-01-24 09:30:05 +0530

    Time: 169 ms (67.94%), Space: 105.7 MB (5%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int minPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        return nums[0]+nums[nums.size()-1];
+        vector<int>sumpairs;
+        int n=nums.size();
+        int l=0,r=n-1;
+        for(int i=0;i<nums.size()/2;i++){
+            sumpairs.push_back(nums[l]+nums[r]);
+            l++;
+            r--;
+        }
+        int maxres=0;
+        for(int it:sumpairs){
+            maxres=max(maxres,it);
+        }
+        return maxres;
     }
 };
