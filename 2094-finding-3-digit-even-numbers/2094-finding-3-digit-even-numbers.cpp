a55e6f8a891021202c773e0ee423f4ff458594d6
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-02-09 14:34:18 +0530

    Time: 554 ms (6.12%), Space: 14.3 MB (26.46%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,26 +1,30 @@
-
 class Solution {
 public:
-    vector<int> findEvenNumbers(vector<int>& digits) {
-        vector<int>mp(10, 0);
-        for(int i=0;i<digits.size();i++){
-            mp[digits[i]]++;
-        }
-            vector<int>res;
-            for(int i=1;i<=9;i++){
-                if(mp[i]==0) continue;
-                mp[i]--;
-                for(int j=0;j<=9;j++){
-                    if(mp[j]==0) continue;
-                    mp[j]--;
-                    for(int k=0;k<=8;k+=2){
-                        if(mp[k]==0) continue;
-                        res.push_back(i*100+j*10+k);
-                    }
-                    mp[j]++;
-                }
-                mp[i]++;
+    void fun(vector<bool>&used,vector<int>&digits,set<int>&st,int len,int num){
+        if(len==3){
+            if(num>=100 && num%2==0){
+                st.insert(num);
             }
+            return;
+        }
+        for(int i=0;i<digits.size();i++){
+            if(!used[i]){
+                 used[i]=true;
+               fun(used,digits,st,len+1,num*10+digits[i]);
+                used[i]=false;
+            }
+           
+        }
+    }
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        set<int>st;
+        int len=0,num=0;
+        vector<bool>used(digits.size(),false);
+        fun(used,digits,st,len,num);
+        vector<int>res;
+        for(auto it:st){
+            res.push_back(it);
+        }
         return res;
     }
 };
