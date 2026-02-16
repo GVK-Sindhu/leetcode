ec2b8c08a24f6c8bbfbb29d0bf2d4e57b36066c5
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-02-16 13:58:28 +0530

    Time: 3 ms (31.14%), Space: 9.9 MB (77.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,23 +3,41 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
+        
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        s.erase(remove_if(s.begin(),s.end(),[](char c){return !isalnum(c);}),s.end());
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+
+    }
+
         // string news="";
         // for(char i:s){
         //     if(isalnum(i)) {
         //         news+=tolower(i);
         //     }
         // }
-        transform(s.begin(),s.end(),s.begin(),::tolower);
-        s.erase(remove_if(s.begin(),s.end(),[](char c){return !isalnum(c);}),s.end());
+    //     transform(s.begin(),s.end(),s.begin(),::tolower);
+    //     s.erase(remove_if(s.begin(),s.end(),[](char c){return !isalnum(c);}),s.end());
 
-        return palindromehelper(s,0);
-    }
-    bool palindromehelper(string &s,int i){
-        int n=s.size();
-        if(i>=n/2) return true;
-        if(s[i]!=s[n-i-1]){
-            return false;
-        }
-        return palindromehelper(s,i+1);
-    }
+    //     return palindromehelper(s,0);
+    // }
+    // bool palindromehelper(string &s,int i){
+    //     int n=s.size();
+    //     if(i>=n/2) return true;
+    //     if(s[i]!=s[n-i-1]){
+    //         return false;
+    //     }
+    //     return palindromehelper(s,i+1);
+     
 };
