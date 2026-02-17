833e3ab3024cd953db2dcbe3c6d0ad5f6ae962d3
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-02-17 20:42:03 +0530

    Time: 0 ms (100%), Space: 12.7 MB (28.77%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int l=0,r=nums.size()-1;
-        while(l<r){
-            int mid=(l+r)/2;
-            if(nums[mid]>nums[mid+1]){
-                r=mid;   
+        int n=nums.size();
+        if(n==1) return 0;
+        else if(nums[0]>nums[1]) return 0;
+        else if(nums[n-1]>nums[n-2]) return n-1;
+        int l=1,h=n-2;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){//peak point
+                return mid;
             }
-            else{
-                l=mid+1;
+            else if(nums[mid]>nums[mid+1]){ //decrease slope
+                h=mid;
+            }
+            else if(nums[mid]<nums[mid+1] && nums[mid-1]<nums[mid]){ //increase slope
+               l=mid+1;
+            }
+            else{//reverse peak point 1 5 1 2 1
+              l=mid+1;
             }
         }
-        return l;
+        return -1;
     }
 };
