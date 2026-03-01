44f6e6ddaa2cbd685bde17d9828306b42d424ad5
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-01 19:37:34 +0530

    Time: 4 ms (28.6%), Space: 16.4 MB (81.74%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int maxdigi='0';
+        int maxi=0;
         for(char c:n){
-            if(c>maxdigi){
-                maxdigi=c;
-            }
+            maxi=max(maxi,c-'0');
         }
-        return maxdigi-'0';
-
+        return maxi;
     }
 };
+
+// 3 -> 1+1+1 9-> all 9 ones
+
+// 1
+// 10
+// 11
+// 100
+// 101
+// 110
+// 111
