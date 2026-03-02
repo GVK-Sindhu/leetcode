198271faeb279b7ccb394cc6b2f87ccd0b81350d
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-02 16:08:17 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    int solve(int idx,vector<int>&nums){
-        if(idx>=nums.size()){
-            return 0;
-        }
-        return max(solve(idx+1,nums,dp),nums[idx]+solve(idx+2,nums,dp));
-    }
+    // int solve(int idx,vector<int>&nums){
+    //     if(idx>=nums.size()){
+    //         return 0;
+    //     }
+    //     return max(solve(idx+1,nums),nums[idx]+solve(idx+2,nums));
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        // vector<int>dp(n,-1);
-        return solve(0,nums,dp);
+        vector<int>dp(n+2);
+        dp[n+1]=0;
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=max(dp[i+1],dp[i+2]+nums[i]);
+        }
+        return dp[0];
     }
 };
