6d695aa8876cc2cf331a2c7c436c556b36d30052
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-08 17:25:55 +0530

    Time: 2 ms (32.8%), Space: 28.3 MB (7.87%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>freq;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        for(auto it:freq){
+            if(it.second>=(n/2+1)){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
