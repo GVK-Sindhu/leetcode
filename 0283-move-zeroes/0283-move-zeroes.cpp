325826d4e73d04c87d28153be97414e4c74b3c20
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-08 17:12:18 +0530

    Time: 0 ms (100%), Space: 24 MB (19.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int nonzero=0;
-        for(int i=0;i<nums.size();i++){
+        int j=0,n=nums.size();
+        for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                swap(nums[i],nums[nonzero]);
-                nonzero++;
+                nums[j]=nums[i];
+                j++;
             }
+        }
+        for(int i=j;i<n;i++){
+            nums[i]=0;
         }
     }
 };
