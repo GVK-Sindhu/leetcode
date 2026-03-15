6dbd212ae3b0c8e1136b38bfc92f3378f59f3d08
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-15 20:34:52 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        vector<int>res(26,0);
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            res[s[i]-'a']++;
-            res[t[i]-'a']--;
+        vector<int>f(26,0);
+        if(s.size()<t.size()){
+            swap(s,t);
         }
-        for(int i:res){
-            if(i!=0) return false;
+        for(int it:s){
+            f[it-'a']++;
+        }
+        for(int it:t){
+            f[it-'a']--;
+        }
+        for(int it:f){
+            if(it>0){
+                return false;
+            }
         }
         return true;
     }
