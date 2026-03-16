c09f60a1f947cbf88ecf7ad871c5df9489f687ab
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-03-16 07:03:45 +0530

    Time: 0 ms (100%), Space: 29.5 MB (67.53%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    void reverse(vector<int>&nums,int i,int j){
-        while(i<j){
-            swap(nums[i],nums[j]);
-            i++;
-            j--;
-        }
-    }
+    // void reverse(vector<int>& nums){
+    //     int 
+    // }
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-            reverse(nums,n-k,n-1);
-            reverse(nums,0,n-k-1);
-            reverse(nums,0,n-1);
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
     }
 };
