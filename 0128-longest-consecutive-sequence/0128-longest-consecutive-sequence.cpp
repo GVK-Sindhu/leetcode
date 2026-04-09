a77684e93916aa370d99cfa66bf70dbd9e02aee7
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-04-09 23:33:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int>s;
-        for(int it:nums){
-            s.insert(it);
-        }
-        int longest=0;
-        int c=0;
-        for(int it:s){
-            if(s.find(it-1)==s.end()){
-                c=1;
-                int x=it;
-                while(s.find(x+1)!=s.end()){
-                    c++;
-                    x=x+1;
+        unordered_set<int>st(nums.begin(),nums.end());
+        int maxlen=0;
+        for(int it:st){  
+            if(st.find(it- 1)==st.end()){
+                int cur=it;
+                int len=1;
+                while(st.find(cur+1)!=st.end()){
+                    cur++;
+                    len++;
                 }
-                longest=max(longest,c);
+                maxlen=max(maxlen,len);
             }
-            
         }
-        return longest;        
+        return maxlen;
     }
 };
