c49ea6a22a73acb9c8793ac2f26af0f2967b0d38
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-04-09 22:21:23 +0530

    Time: 32 ms (92.6%), Space: 45.4 MB (54.49%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+         int c=0,s=0;
+         unordered_map<int,int>freq;
+         freq[0]=1;
+         for(int i=0;i<nums.size();i++){
+            s+=nums[i];
+            if(freq.find(s-k)!=freq.end()){
+                c+=freq[s-k];
+            }
+            freq[s]++;
+         }
+         return c;
+    }
+};
