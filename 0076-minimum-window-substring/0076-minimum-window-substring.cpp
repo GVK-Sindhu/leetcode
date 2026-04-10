58f12e4272fc6c933bbbfe3c6082868304543fde
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-04-10 21:19:44 +0530

    Time: 0 ms (100%), Space: 11.3 MB (80.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-    bool isgood(vector<int>&f1,vector<int>&f2){
-        for(int i=0;i<128;i++){
-            if(f2[i]<f1[i]){
-                return false;
-            }
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<int>f1(128);
-        vector<int>f2(128);
-        int minlen=INT_MAX;
-        for(int i=0;i<m;i++){
-            f1[t[i]]++;
+        vector<int>hash(256,0);
+        for(char c:t){
+            hash[c]++;
         }
-        int st=-1,ed=-1;
-        int i=0;
-        for(int j=0;j<n;j++){
-            f2[s[j]]++;
-            while(isgood(f1,f2)){
-                if((j-i+1)<minlen){
-                    st=i;
-                    ed=j;
-                    minlen=(j-i+1);
+        int l=0,need=t.size();
+        int minlen=INT_MAX,start=0;
+        for(int r=0;r<s.size();r++){
+            if(hash[s[r]]>0) need--;
+            hash[s[r]]--;
+            while(need==0){
+                if((r-l+1)<minlen){
+                    minlen=r-l+1;
+                    start=l;
                 }
-                f2[s[i]]--;
-                i++;
+                hash[s[l]]++;
+                if(hash[s[l]]>0) need++;
+                l++;
             }
         }
-        string res;
-        if(st==-1 || ed==-1){
-            return res;
-        }
-        else{
-            for(int i=st;i<=ed;i++){
-                res+=s[i];
-            }
-        }
-        return res;
+        return minlen==INT_MAX?"":s.substr(start,minlen);
     }
 };
