e0f9bd388c0fa4aba1eaba7062517d565abd7a0b
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-04 22:55:30 +0530

    Time: 0 ms (100%), Space: 17.4 MB (88.07%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,48 @@
 class Solution {
 public:
+    int solvel(int n,vector<int>&nums,int target){
+        int l=0,r=n-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                r=mid-1;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+    int solver(int n,vector<int>&nums,int target){
+        int l=0,r=n-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                l=mid+1;
+            }
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
         int n=nums.size();
-        int start=-1,end=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                start=i;
-                break;
-            }
-        }
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]==target){
-                end=i;
-                break;
-            }
-        }
-        return {start,end};
-        // int l=0,r=n-1;
-        // int res=-1,res1=-1;
-        // vector<int>ans;
-        // while(l<=r){
-        //     int mid=(l+(r-l))/2;
-        //     if(nums[mid]==target){
-        //          res=mid;
-        //     }
-        //     else if(nums[mid]<target){
-        //         l=mid+1;
-        //     }
-        //     else{
-        //         r=mid-1;
-        //     }
-        // }
-        // ans.push_back(res);
-        // ans.push_back(res1);
-        // return ans;
+        vector<int>res;
+        int sl=solvel(n,nums,target);
+        int sr=solver(n,nums,target);
+        res.push_back(sl);
+        res.push_back(sr);
+        return res;
     }
 };
