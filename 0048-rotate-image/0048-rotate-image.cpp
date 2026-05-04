0945dc28d688d919eec77bc7b14e6558482549c5
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-04 21:45:10 +0530

    Time: 0 ms (100%), Space: 10.3 MB (35.34%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=i+1;j<col;j++){
-                swap(matrix[i][j],matrix[j][i]);
+    void rotate(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<m;j++){
+               swap(mat[i][j],mat[j][i]);
             }
         }
-        for(int i=0;i<row;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
+        for(int i=0;i<n;i++){
+            reverse(mat[i].begin(),mat[i].end());
         }
     }
 };
