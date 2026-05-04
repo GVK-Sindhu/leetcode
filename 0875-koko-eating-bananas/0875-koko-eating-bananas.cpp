1676e73e5295b6197e66ef2a1ccc34734bd2fe3d
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-04 17:59:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    bool solve(int mid,vector<int>&piles,int hr){
+    bool solve(vector<int>&piles,long long mid,long long hr)
+    {
         long long reqhr=0;
         for(int it:piles){
             reqhr+=(it+mid-1)/mid;
         }
         return reqhr<=hr;
     }
-    int minEatingSpeed(vector<int>& piles, int hr) {
+    int minEatingSpeed(vector<int>& piles, long long hr) {
         long long l=1,h=*max_element(piles.begin(),piles.end());
-        long long minres=INT_MAX;
+        long long res=INT_MAX;
         while(l<=h){
             long long mid=l+(h-l)/2;
-            if(solve(mid,piles,hr)){
-                minres=min(minres,mid);
-                h=mid-1;
+            if(solve(piles,mid,hr)){
+               res=min(res,mid);
+               h=mid-1;
             }
             else{
                 l=mid+1;
             }
         }
-        return minres;
+        return res;
     }
 };
