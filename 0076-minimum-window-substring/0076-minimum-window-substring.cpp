b258cfc2f1918a672628e205ed90dcd392a80c57
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-05 15:05:22 +0530

    Time: 0 ms (100%), Space: 11.4 MB (62.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       int minlen=INT_MAX;
-       int l=0,r=0,start=0;
-       int need=t.size();
-       vector<int>hash(256,0);
-       for(char c:t){
-        hash[c]++;
-       }
-       while(r<s.size()){
-         if(hash[s[r]]>0) need--;
-         hash[s[r]]--;
-         while(need==0){
-            if((r-l+1)<minlen){
-                minlen=(r-l+1);
-                start=l;
+        int n=s.size();
+        int need=t.size();
+        int minlen=INT_MAX;
+        vector<int>hash(256,0);
+        int l=0,r=0,start=0;
+        for(char c:t){
+            hash[c]++;
+        }
+        while(r<n){
+            if(hash[s[r]]>0){
+                need--;
             }
-            hash[s[l]]++;
-            if(hash[s[l]]>0) need++;
-            l++;
-         }
-         r++;
-       }
-       return minlen==INT_MAX?"":s.substr(start,minlen);
+            hash[s[r]]--;
+            while(need==0){
+                if((r-l+1)<minlen){
+                    minlen=(r-l+1);
+                    start=l;
+                }
+                hash[s[l]]++;
+                if(hash[s[l]]>0){
+                    need++;
+                }
+                l++;
+            }
+            r++;
+        }
+        return minlen==INT_MAX?"":s.substr(start,minlen);
+
     }
 };
