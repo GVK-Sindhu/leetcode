9c96ae871f299fa533b6848a6c6f7aba41570790
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-05 14:20:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-       int n=s1.size();
-       int m=s2.size();
-       int i=0,j=0;
-       vector<int>c1(26,0);
-       for(int i=0;i<n;i++){
-         c1[s1[i]-'a']++;
+       int n1=s1.size();
+       vector<int>freq1(26,0);
+       vector<int>freq2(26,0);
+       for(int i=0;i<n1;i++){
+        freq1[s1[i]-'a']++;
        }
-       vector<int>c2(26,0);
-       while(j<m){
-        c2[s2[j]-'a']++;
-         while((j-i+1)>n){
-            c2[s2[i]-'a']--;
-            i++;
-         }
-         if(c1==c2){
+       for(int i=0;i<s2.size();i++){
+        freq2[s2[i]-'a']++;
+          if(i>=s1.size()){
+            freq2[s2[i-s1.size()]-'a']--;
+          }
+          if(freq1==freq2){
             return true;
-         }
-         j++;
+          }
        }
        return false;
+
     }
 };
