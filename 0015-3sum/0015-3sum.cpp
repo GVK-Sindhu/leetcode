2d5891570089ba0c4ee9062318e480d4da7526d4
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-05 11:04:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,9 +5,9 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>>res;
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=n-1;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             while(j<k){
                 int s=nums[i]+nums[j]+nums[k];
                 if(s==0){
@@ -15,7 +15,7 @@ public:
                     j++;
                     k--;
                     while(j<k && nums[j]==nums[j-1]) j++;
-                    while(j<k && nums[k]==nums[k+1]) k--;
+                    while(j<k && nums[k]==nums[k+1]) k++;
                 }
                 else if(s<0){
                     j++;
