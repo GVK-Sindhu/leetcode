6299ff80135cd5c77e4cbe94160f4916136c958e
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-05 11:16:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -3,12 +3,13 @@ public:
     int maxArea(vector<int>& height) {
         int n=height.size();
         int l=0,r=n-1;
+        int maxhl=0,maxhr=0;
         int maxarea=0;
-        while(l<=r){
-            int h=min(height[l],height[r]);
-            int w=r-l;
-            int area=w*h;
-            maxarea=max(area,maxarea);
+        while(l<r){
+            int heights=min(height[l],height[r]);
+            int width=(r-l);
+            int area=heights*width;
+            maxarea=max(maxarea,area);
             if(height[l]<=height[r]){
                 l++;
             }
