a061ff3d01041951b24bab28de4dd18d066f701d
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-06 23:17:03 +0530

    Time: 3 ms (89.33%), Space: 23.7 MB (81.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
         vector<vector<int>>res;
-        for(auto it:intervals){
-            if(res.empty() || res.back()[1]<it[0]){
-                res.push_back(it);
+        sort(intervals.begin(),intervals.end());
+        int n=intervals.size();
+        res.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=res.back()[1]){
+                res.back()[1]=max(intervals[i][1],res.back()[1]);
             }
             else{
-                res.back()[1]=max(res.back()[1],it[1]);
+                res.push_back(intervals[i]);
             }
         }
         return res;
