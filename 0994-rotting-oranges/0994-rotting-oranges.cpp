cbab794d54a27a291836599995d8cf65bfa63209
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-06 10:55:17 +0530

    Time: 4 ms (15.54%), Space: 17.2 MB (28.68%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,22 +5,23 @@ public:
         int m=grid[0].size();
         queue<pair<pair<int,int>,int>>q;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        int cntfreshorg=0;
+        int freshcnt=0;
+        int t=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
                     vis[i][j]=2;
+                    q.push({{i,j},t});
                 }
                 if(grid[i][j]==1){
-                    cntfreshorg++;
+                    freshcnt++;
                 }
             }
         }
         int tm=0;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
         int cnt=0;
+        int delr[]={-1,0,1,0};
+        int delc[]={0,-1,0,1};
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
@@ -28,20 +29,19 @@ public:
             q.pop();
             tm=max(tm,t);
             for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m
-                && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
+            int newr=r+delr[i];
+            int newc=c+delc[i];
+                if(newr<n && newr>=0 && newc>=0 && newc<m
+                && grid[newr][newc]==1 && vis[newr][newc]!=2){
                     cnt++;
+                    vis[newr][newc]=2;
+                    q.push({{newr,newc},t+1});
                 }
             }
         }
-        if(cnt!=cntfreshorg){
+        if(cnt!=freshcnt){
             return -1;
         }
         return tm;
-
     }
 };
