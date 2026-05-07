fe8a6f7db3109a6da0bd29186f6828ecd1ff6c9b
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-07 15:38:15 +0530

    Time: 29 ms (52.26%), Space: 36.6 MB (52.87%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int idx,vector<int>&coins,int amount,vector<vector<int>>&dp){
+        if(idx==0){
+            return (amount%coins[0]==0);
+        }
+        if(dp[idx][amount]!=-1) return dp[idx][amount];
+        int nottake=solve(idx-1,coins,amount,dp);
+        int take=0;
+        if(coins[idx]<=amount){
+            take=solve(idx,coins,amount-coins[idx],dp);
+        }
+        return dp[idx][amount]=take+nottake;
+    }
+    int change(int amount, vector<int>& coins) {
+         int n=coins.size();
+         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return solve(n-1,coins,amount,dp);
+
+    }
+};
