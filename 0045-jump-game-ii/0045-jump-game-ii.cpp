5335a0642e6271776dcee8a374f48fe4af3c3d43
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-07 22:48:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-        int l=0,r=0;
-        int farthest=0;
-        int jumps=0;
+        int l=0,r=0,far=0,jumps=0;
+        //check valid window like check farthest point is taken and l= valid window+1; r=farthest
         while(r<n-1){
             for(int i=l;i<=r;i++){
-                farthest=max(farthest,i+nums[i]);
+                far=max(far,i+nums[i]);
             }
             l=r+1;
-            r=farthest;
+            r=far;
             jumps++;
         }
         return jumps;
