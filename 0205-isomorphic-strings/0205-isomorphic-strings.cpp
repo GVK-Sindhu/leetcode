00f6c924ca44214ff481d7a58bb426a4440b5128
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-21 21:18:31 +0530

    Time: 0 ms (100%), Space: 9.3 MB (55.25%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>freq1;
-        unordered_map<char,char>freq2;
-        for(int i=0;i<s.size();i++){
-            if(freq1[s[i]] && freq1[s[i]]!=t[i]){
-                return false;
-            }
-            if(freq2[t[i]] && freq2[t[i]]!=s[i]){
-                return false;
-            }
-            freq1[s[i]]=t[i];
-            freq2[t[i]]=s[i];
+        // unordered_map<char,char>freq1;
+        // unordered_map<char,char>freq2;
+        // for(int i=0;i<s.size();i++){
+        //     if(freq1[s[i]] && freq1[s[i]]!=t[i]){
+        //         return false;
+        //     }
+        //     if(freq2[t[i]] && freq2[t[i]]!=s[i]){
+        //         return false;
+        //     }
+        //     freq1[s[i]]=t[i];
+        //     freq2[t[i]]=s[i];
+        // }
+        // return true;
+        int m1[256]={0},m2[256]={0};
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(m1[s[i]]!=m2[t[i]]) return false;
+            m1[s[i]]=i+1;
+            m2[t[i]]=i+1;
         }
         return true;
     }
