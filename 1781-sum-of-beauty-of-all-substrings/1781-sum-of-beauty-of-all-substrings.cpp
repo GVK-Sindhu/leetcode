82e58d3f7dfa54d9e7a0e78f0d24cfb9ea945171
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-22 12:46:56 +0530

    Time: 115 ms (51.52%), Space: 26 MB (31.39%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -17,10 +17,18 @@ public:
     int beautySum(string s) {
         int sum=0;
         for(int i=0;i<s.size();i++){
-              string tmp="";
+            //   string tmp="";
+            unordered_map<char,int>freq;
             for(int j=i;j<s.size();j++){
-                tmp+=s[j];
-                sum+=fun(tmp);
+                // tmp+=s[j];
+                // sum+=fun(tmp);
+                freq[s[j]]++;
+                int minf=INT_MAX,maxf=0;
+                for(auto it:freq){
+                    minf=min(minf,it.second);
+                    maxf=max(maxf,it.second);
+                }
+                sum+=(maxf-minf);
             }
         }
         return sum;
