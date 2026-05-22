b3a8c2b7fdbbc145e92512d623fe18aa868ba1af
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-22 12:15:34 +0530

    Time: 0 ms (100%), Space: 11.6 MB (35.91%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char,int>m;
+        map<char,int>freq;
         for(int i=0;i<s.size();i++){
-            m[s[i]]++;
+            freq[s[i]]++;
         }
-        vector<pair<char,int>>v(m.begin(),m.end());
-        sort(v.begin(),v.end(),[](const auto& a,const auto& b){   
-              return a.second>b.second;
-         });
-        string res;
-        for(auto it:v){
-           res+=string(it.second,it.first);
+        vector<pair<char,int>>arr(freq.begin(),freq.end());
+        sort(arr.begin(),arr.end(),[](pair<char,int>a,pair<char,int>b){
+            return a.second>b.second;
+        });
+        string res="";
+        for(auto it:arr){
+            res+=string(it.second,it.first);
         }
         return res;
-
-        
     }
 };
