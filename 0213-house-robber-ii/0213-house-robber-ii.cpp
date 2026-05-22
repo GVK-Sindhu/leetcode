afea9eeb425e1489617a9cec1e7c1eb73183444a
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-22 16:50:18 +0530

    Time: 0 ms (100%), Space: 10.4 MB (61.13%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int st,int ed,vector<int>&nums){
+        int prev1=nums[st];
+        int prev2=0;
+        for(int i=st+1;i<=ed;i++){
+            int take=nums[i];
+            if(i>=st) take+=prev2;
+            int nottake=prev1;
+            int curi=max(take,nottake);
+            prev2=prev1;
+            prev1=curi;
+        }
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size()-1;
+        if(nums.size()==1) return nums[0];
+        int s1=solve(1,n,nums);
+        int s2=solve(0,n-1,nums);
+        return max(s1,s2);
+    }
+};
