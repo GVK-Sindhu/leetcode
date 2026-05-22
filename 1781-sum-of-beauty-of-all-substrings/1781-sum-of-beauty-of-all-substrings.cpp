396ecac5d324e586150132823623f76e61f53afd
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-22 12:41:51 +0530

    Time: 1465 ms (5.01%), Space: 526.3 MB (5.01%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int fun(string tmp){
+        vector<int>freq(26,0);
+        for(int i=0;i<tmp.size();i++){
+            freq[tmp[i]-'a']++;
+        }
+        int maxf=0,minf=INT_MAX;
+        for(int it:freq){
+            if(it>0){
+                maxf=max(maxf,it);
+                minf=min(minf,it);
+            }
+        }
+        return maxf-minf;
+    }
+    int beautySum(string s) {
+        int sum=0;
+        for(int i=0;i<s.size();i++){
+              string tmp="";
+            for(int j=i;j<s.size();j++){
+                tmp+=s[j];
+                sum+=fun(tmp);
+            }
+        }
+        return sum;
+    }
+};
