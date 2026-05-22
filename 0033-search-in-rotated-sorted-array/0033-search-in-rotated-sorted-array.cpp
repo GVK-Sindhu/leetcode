cc0204fcd8e3f5e66cc4c8aa827fefc5ddc4bc7f
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-22 09:38:18 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int l=0,h=n-1;
+        int l=0,h=nums.size()-1;
         while(l<=h){
-            int mid=(l+h)/2;
+            int mid=l+(h-l)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[mid]>=nums[l]){
-                if(target>=nums[l] && target<=nums[mid]){
-                    h=mid-1;
-                }
-                else{
-                    l=mid+1;
-                }
-            }
-            else{
-                if(target>=nums[mid] && target<=nums[h]){
-                    l=mid+1;
-                }
-                else{
-                    h=mid-1;
-                }
-            }
+           if(nums[mid]<=nums[h]){ //right half sorted
+              if(target>=nums[mid] && target<=nums[h]){
+                l=mid+1;
+              }
+              else{
+                h=mid-1;
+              }
+           }
+           else{
+              if(target<=nums[mid] && target>=nums[l]){
+                 h=mid-1;
+              }
+              else{
+                l=mid+1;
+              }
+           }
         }
         return -1;
     }
