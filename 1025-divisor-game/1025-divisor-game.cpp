e016fb133c1171459280a2dbc17562078501e9e0
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-22 21:04:10 +0530

    Time: 0 ms (100%), Space: 7.8 MB (26.04%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        // int c=0;
-        // while(n>0){
-        // if(n%2==0){
-        //     n=n-1;
-        //     c++;
-        // }
-        // else{
-        //     n=n-2;
-        //     c++;
-        // }
-        // }
-        if(n%2==0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (n&1)==0;
     }
 };
