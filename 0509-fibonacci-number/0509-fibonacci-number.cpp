d613f256c7ba07b71726dbc523b8add8e465bfce
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-22 20:33:24 +0530

    Time: 3 ms (62.24%), Space: 7.6 MB (88.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,13 +4,13 @@ public:
         if(n<=1){
             return n;
         }
-        vector<int>dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
+        int prev2=0;
+        int prev1=1;
         for(int i=2;i<=n;i++){
-            dp[i]+=dp[i-1];
-            if(i>1) dp[i]+=dp[i-2];
+            int curi=prev1+prev2;
+            prev2=prev1;
+            prev1=curi;
         }
-        return dp[n];
+        return prev1;
     }
 };
