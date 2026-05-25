f4b39243caf80d6d96663bd23f32753810818cb2
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-25 11:50:48 +0530

    Time: 0 ms (100%), Space: 30.1 MB (32.82%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& grp) {
         vector<string>res;
-        int n=words.size();
-        if(n==0) return res;
+        int prev=grp[0];
         res.push_back(words[0]);
-        int prevgrp=groups[0];
-        for(int i=1;i<n;i++){
-            if(prevgrp!=groups[i]){
+        for(int i=1;i<grp.size();i++){
+            if(grp[i]!=prev){
+                prev=grp[i];
                 res.push_back(words[i]);
             }
-            prevgrp=groups[i];
         }
         return res;
     }
