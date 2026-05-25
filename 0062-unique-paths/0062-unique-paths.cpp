c267515914944d2edbda735a7f40db350ad57454
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-25 14:13:26 +0530

    Time: 2 ms (20.63%), Space: 8.3 MB (74.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,19 +9,39 @@ public:
         return dp[i][j]=up+left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // memoization
         // return solve(m-1,n-1,dp);
+
+        // tabulation
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0) dp[i][j]=1;
+        //         else{
+        //              int up=0,left=0;
+        //             if(i>0) up=dp[i-1][j];
+        //             if(j>0) left=dp[i][j-1];
+        //             dp[i][j]=up+left;
+        //         }
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        // space optimizaiton
+        vector<int>prev(n+1,-1);
         for(int i=0;i<m;i++){
+            vector<int>cur(n+1,-1);
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j]=1;
+                if(i==0 && j==0) cur[j]=1;
                 else{
                     int up=0,left=0;
-                    if(i>0) up=dp[i-1][j];
-                    if(j>0) left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                    if(i>0)  up=prev[j];
+                    if(j>0)  left=cur[j-1];
+                    cur[j]=up+left;
                 }
             }
+            prev=cur;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
