2812d7114d2b4b9ba21677b6ea4c24d2ccff05c5
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-25 15:49:03 +0530

    Time: 2 ms (13.3%), Space: 12 MB (6.05%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -13,7 +13,19 @@ public:
         int r=grid.size();
         int c=grid[0].size();
         vector<vector<int>>dp(r,vector<int>(c,-1));
-        return solve(r-1,c-1,grid,dp);
-
+        // return solve(r-1,c-1,grid,dp);
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0 && grid[i][j]!=1) dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0)  up=dp[i-1][j];
+                    if(j>0)  left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[r-1][c-1];
     }
 };
