c117d8818fc680cf5e273feee08c0d1dcfdebf22
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-25 11:30:04 +0530

    Time: 0 ms (100%), Space: 10.6 MB (78.54%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -13,10 +13,53 @@ public:
         return c;
     }
     vector<int> countBits(int n) {
-        vector<int>res(n+1,0);
-        for(int i=0;i<=n;i++){
-            res[i]=fun(i);
+        // vector<int>res(n+1,0);
+        // for(int i=0;i<=n;i++){
+        //     res[i]=fun(i);
+        // }
+        // return res;
+        vector<int>dp(n+1,0);
+        for(int i=1;i<=n;i++){
+            dp[i]=dp[i>>1]+(i&1);
         }
-        return res;
+        return dp;
     }
 };
+
+// Main DP Relation
+// For any number i:
+
+// dp[i]=dp[i/2]+(i%2)
+
+// or bitwise:
+
+// dp[i]=dp[i>>1]+(i&1)
+
+// Here:
+// i >> 1 = remove last binary bit
+// i & 1 = tells whether last bit is 1 or 0
+// Example:
+
+// i = 13
+
+// Binary:
+
+// 13 = 1101
+
+// Now:
+
+// 13 >> 1 = 110 = 6
+
+// We already know:
+
+// dp[6] = 2
+
+// Last bit:
+
+// 13 & 1 = 1
+
+// So:
+
+// dp[13] = dp[6] + 1
+//        = 2 + 1
+//        = 3
