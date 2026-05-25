1e5c821184f6b5d80af119b5804f7756da71c1dd
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-25 16:01:32 +0530

    Time: 0 ms (100%), Space: 11.5 MB (84.24%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -12,20 +12,36 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        vector<vector<int>>dp(r,vector<int>(c,-1));
+        // vector<vector<int>>dp(r,vector<int>(c,-1));
         // return solve(r-1,c-1,grid,dp);
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         if(grid[i][j]==1) dp[i][j]=0;
+        //         else if(i==0 && j==0 && grid[i][j]!=1) dp[i][j]=1;
+        //         else{
+        //             int up=0,left=0;
+        //             if(i>0)  up=dp[i-1][j];
+        //             if(j>0)  left=dp[i][j-1];
+        //             dp[i][j]=up+left;
+        //         }
+        //     }
+        // }
+        // return dp[r-1][c-1];
+        vector<int>prv(c,-1);
         for(int i=0;i<r;i++){
+            vector<int>cur(c,-1);
             for(int j=0;j<c;j++){
-                if(grid[i][j]==1) dp[i][j]=0;
-                else if(i==0 && j==0 && grid[i][j]!=1) dp[i][j]=1;
+                if(grid[i][j]==1) cur[j]=0;
+                else if(i==0 && j==0) cur[j]=1;
                 else{
                     int up=0,left=0;
-                    if(i>0)  up=dp[i-1][j];
-                    if(j>0)  left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                    if(i>0) up=prv[j];
+                    if(j>0) left=cur[j-1];
+                    cur[j]=up+left;
                 }
             }
+            prv=cur;
         }
-        return dp[r-1][c-1];
+        return prv[c-1];
     }
 };
