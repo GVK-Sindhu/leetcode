7a30fbca76ee033f33921ecde3aa00235e53cf99
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-25 12:02:08 +0530

    Time: 0 ms (100%), Space: 9.6 MB (62.49%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>res(numRows);
+        vector<vector<int>>ans;
         for(int i=0;i<numRows;i++){
-            res[i]=vector<int>(i+1,1);
+            vector<int>row(i+1,1);
             for(int j=1;j<i;j++){
-                res[i][j]=res[i-1][j]+res[i-1][j-1];
+                row[j]=ans[i-1][j-1]+ans[i-1][j];
             }
+            ans.push_back(row);
         }
-        return res;
+        return ans;
     }
 };
