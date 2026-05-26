56f7a5f7b24137f6c0c05ec713a5bf797c6f0978
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-26 10:02:28 +0530

    Time: 0 ms (100%), Space: 16.8 MB (14.49%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -13,6 +13,38 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int r=grid.size(),c=grid[0].size();
         vector<vector<int>>dp(r,vector<int>(c,-1));
-        return solve(r-1,c-1,grid,dp);
+        // return solve(r-1,c-1,grid,dp);
+       
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         if(i==0 && j==0) {
+        //              dp[i][j]=grid[i][j];
+        //         }
+        //         else{
+        //             int up=1e9,left=1e9;
+        //             if(i>0) up=grid[i][j]+dp[i-1][j];
+        //             if(j>0) left=grid[i][j]+dp[i][j-1];
+        //             dp[i][j]=min(up,left);
+        //         }
+        //     }
+        // }
+        // return dp[r-1][c-1];
+
+        // space optimization
+        vector<int>prev(c,1e9);
+        for(int i=0;i<r;i++){
+            vector<int>cur(c,-1);
+            for(int j=0;j<c;j++){
+                if(i==0 && j==0) cur[j]=grid[i][j];
+                else{
+                    int up=1e9,left=1e9;
+                    if(i>0) up=grid[i][j]+prev[j];
+                    if(j>0) left=grid[i][j]+cur[j-1];
+                    cur[j]=min(up,left);
+                }
+            }
+            prev=cur;
+        }
+        return prev[c-1];
     }
 };
