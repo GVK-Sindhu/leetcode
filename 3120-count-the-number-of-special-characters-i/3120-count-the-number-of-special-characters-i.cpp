d6d8312980847d253fc84c411b8f298e3cfd1910
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-26 09:25:00 +0530

    Time: 0 ms (100%), Space: 8.9 MB (78.89%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        unordered_map<char,int>freq;
-        set<char>st;
-        for(auto it:word){
-            freq[it]++;
-            char tmp=tolower(it);
-            st.insert(tmp);
+        vector<bool>lower(26,false);
+        vector<bool>upper(26,false);
+        for(int i=0;i<word.size();i++){
+            if(word[i]>='a' && word[i]<='z'){
+                lower[word[i]-'a']=true;
+            }
+            if(word[i]>='A' && word[i]<='Z'){
+                upper[word[i]-'A']=true;
+            }
         }
         int c=0;
-        for(auto it:st){
-            char tmp=toupper(it);
-            if(freq[tmp]>0 && freq[it]>0){
+        for(int i=0;i<26;i++){
+            if(lower[i] && upper[i]){
                 c++;
             }
         }
