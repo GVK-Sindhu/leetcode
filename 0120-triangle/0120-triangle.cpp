1ed6e8e25ea70aabd0be449937e94dd8745ae264
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-26 10:44:14 +0530

    Time: 0 ms (100%), Space: 13.4 MB (22.2%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>&dp,vector<vector<int>>& triangle){
+        int r=triangle.size();
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==r-1){
+            return dp[r-1][j]=triangle[r-1][j];
+        }
+        int bottom=triangle[i][j]+solve(i+1,j,dp,triangle);
+        int right=triangle[i][j]+solve(i+1,j+1,dp,triangle);
+        return dp[i][j]=min(bottom,right);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int r=triangle.size();
+        vector<vector<int>>dp(r,vector<int>(r,-1));
+        // return solve(0,0,dp,triangle);
+        for(int j=0;j<r;j++){
+            dp[r-1][j]=triangle[r-1][j];
+        }
+        for(int i=r-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int bottom=triangle[i][j]+dp[i+1][j];
+                int right=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(bottom,right);
+            }
+        }
+        return dp[0][0];
+    }
+};
