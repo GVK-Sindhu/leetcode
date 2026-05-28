f7624c606540ea811fedb771788623a152537ee7
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-28 14:53:20 +0530

    Time: 47 ms (30.98%), Space: 57.3 MB (5.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -24,26 +24,26 @@ public:
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
         // return solve(n-1,coins,amount,dp)>=1e9?-1:solve(n-1,coins,amount,dp);
-        for(int i=1;i<=amount;i++){
-            if(i%coins[0]==0){
-                dp[0][i]=i/coins[0];
-            }
-            else{
-                dp[0][i]=1e9;
-            }
-        }
-        for(int i=0;i<coins.size();i++){
-            dp[i][0]=0;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=amount;j++){
-                int nottake=dp[i-1][j];
-                int take=1e9;
-                if(coins[i]<=j) take=1+dp[i][j-coins[i]];
-                dp[i][j]=min(take,nottake);
-            }
-        }
-        return dp[n-1][amount]>=1e9?-1:dp[n-1][amount];
+        // for(int i=1;i<=amount;i++){
+        //     if(i%coins[0]==0){
+        //         dp[0][i]=i/coins[0];
+        //     }
+        //     else{
+        //         dp[0][i]=1e9;
+        //     }
+        // }
+        // for(int i=0;i<coins.size();i++){
+        //     dp[i][0]=0;
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<=amount;j++){
+        //         int nottake=dp[i-1][j];
+        //         int take=1e9;
+        //         if(coins[i]<=j) take=1+dp[i][j-coins[i]];
+        //         dp[i][j]=min(take,nottake);
+        //     }
+        // }
+        // return dp[n-1][amount]>=1e9?-1:dp[n-1][amount];
 
         // space optimization
         vector<int>prev(amount+1,-1),cur(amount+1,-1);
