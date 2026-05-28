100fa045bd56a4a39d0645dea2788215c3570ada
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-28 14:39:17 +0530

    Time: 45 ms (33.44%), Space: 49.3 MB (26.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int solve(int idx,vector<int>&coins,int amount,vector<vector<int>>&dp){
-        if(idx==0){
-            if(amount % coins[0] == 0)
-                return amount / coins[0];
-            return 1e9;
+    int solve(int i,vector<int>& nums, int tar,vector<vector<int>>&dp){
+        if(tar==0){
+            return 0;
         }
-        if(dp[idx][amount]!=-1){
-            return dp[idx][amount];
+        if(i==0){
+            if(tar%nums[i]==0){
+                return tar/nums[i];
+            }
+            else{
+                return 1e9;
+            }
         }
-        int nottake=solve(idx-1,coins,amount,dp);
+        if(dp[i][tar]!=-1){
+            return dp[i][tar];
+        }
+        int nottake=solve(i-1,nums,tar,dp);
         int take=1e9;
-        if(coins[idx]<=amount){
-            take=1+solve(idx,coins,amount-coins[idx],dp);
-        }
-        return dp[idx][amount]=min(take,nottake);
+        if(nums[i]<=tar) take=1+solve(i,nums,tar-nums[i],dp);
+        return dp[i][tar]=min(take,nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int res=solve(n-1,coins,amount,dp);
-        return (res>=1e9)?-1:res;
+        return solve(n-1,coins,amount,dp)>=1e9?-1:solve(n-1,coins,amount,dp);
     }
 };
