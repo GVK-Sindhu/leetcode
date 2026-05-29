0d5e92030a0a10c569f22cdb861a529668290420
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-05-29 12:21:46 +0530

    Time: 7 ms (66.64%), Space: 15.5 MB (47.48%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(int i,vector<int>& nums,int tar,vector<vector<int>>&dp){
+        // if(tar==0){
+        //     return 1;
+        // }
+        if(i==0){
+            if(nums[i]==0 && tar==0) return 2;
+            if(tar==0 || tar==nums[i]) return 1;
+            return 0;
+        }
+        if(dp[i][tar]!=-1){
+            return dp[i][tar];
+        }
+        int notpick=solve(i-1,nums,tar,dp);
+        int pick=0;
+        if(tar>=nums[i]){
+            pick=solve(i-1,nums,tar-nums[i],dp);
+        }
+        return dp[i][tar]=pick+notpick;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int ts=0;
+        for(int it:nums){
+            ts+=it;
+        }
+        if((ts-target)<0 || (ts-target)%2!=0) return 0;
+        int tar=(ts-target)/2;
+        vector<vector<int>>dp(n,vector<int>(tar+1,-1));
+        return solve(n-1,nums,tar,dp);
+    }
+};
