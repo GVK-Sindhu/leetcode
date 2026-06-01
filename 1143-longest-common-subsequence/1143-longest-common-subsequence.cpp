94048b7e7679bfa920d32f3585bb44920f8becbf
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-01 17:53:49 +0530

    Time: 14 ms (93.42%), Space: 9.1 MB (96.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -11,26 +11,44 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int len1=text1.size();
         int len2=text2.size();
-        vector<vector<int>>dp(len1+1,vector<int>(len2+1,-1));
-        // return solve(len1-1,len2-1,text1,text2,dp);
-        for(int j=0;j<=len2;j++){
-            dp[0][j]=0;
-        }
-        for(int i=0;i<=len1;i++){
-            dp[i][0]=0;
-        }
+        // vector<vector<int>>dp(len1+1,vector<int>(len2+1,-1));
+        // // return solve(len1-1,len2-1,text1,text2,dp);
+        // for(int j=0;j<=len2;j++){
+        //     dp[0][j]=0;
+        // }
+        // for(int i=0;i<=len1;i++){
+        //     dp[i][0]=0;
+        // }
 
+        // for(int i=1;i<=len1;i++){
+        //     for(int j=1;j<=len2;j++){
+        //         if(text1[i-1]==text2[j-1]){
+        //             dp[i][j]=1+dp[i-1][j-1];
+        //         }
+        //         else{
+        //             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        //         }
+        //     }
+        // }
+        // return dp[len1][len2];
+
+        // space optimize
+        vector<int>prev(len2+1,0),cur(len2+1,0);
+        for(int j=0;j<=len2;j++){
+            prev[j]=0;
+        }
         for(int i=1;i<=len1;i++){
             for(int j=1;j<=len2;j++){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                   cur[j]=1+prev[j-1];
                 }
                 else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    cur[j]=max(prev[j],cur[j-1]);
                 }
             }
+            prev=cur;
         }
-        return dp[len1][len2];
+        return prev[len2];
 
     }
 };
