290cfa10ff01ea5e769b3b5875208e31a9db1d07
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-02 19:02:42 +0530

    Time: 0 ms (100%), Space: 97.5 MB (24.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int minprice=prices[0];
+        int n=prices.size();
         int maxprofit=0;
-        for(int i=1;i<prices.size();i++){
+        for(int i=0;i<n;i++){
             if(prices[i]<minprice){
                 minprice=prices[i];
             }
-            else{
-                maxprofit=max(maxprofit,prices[i]-minprice);
-            }
+            maxprofit=max(maxprofit,prices[i]-minprice);
         }
         return maxprofit;
     }
