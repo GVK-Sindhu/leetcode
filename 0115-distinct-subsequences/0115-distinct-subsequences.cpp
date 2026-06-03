bc11161d75206026b13191268483147d1265e543
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-03 16:12:12 +0530

    Time: 28 ms (60.76%), Space: 44.4 MB (21.32%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -16,7 +16,22 @@ public:
     int numDistinct(string s, string t) {
         int l1=s.size();
         int l2=t.size();
-        vector<vector<int>>dp(l1,vector<int>(l2,-1));
-        return solve(l1-1,l2-1,s,t,dp);
+        vector<vector<unsigned long long>>dp(l1+1,vector<unsigned long long>(l2+1,0));
+        // return solve(l1-1,l2-1,s,t,dp);
+        for(int i=0;i<=l1;i++){
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[l1][l2];
     }
 };
