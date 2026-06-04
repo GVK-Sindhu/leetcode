52908d3630c2eabc34646d41f757ad67eb84f960
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-04 15:03:04 +0530

    Time: 54 ms (49.39%), Space: 15.7 MB (68.38%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
-    bool isMatch(string inp, string tar) {
-        int startidx=-1,match=0;
-        int i=0,j=0;
-        while(i<inp.size()){
-            if(j<tar.size() && (inp[i]==tar[j] || tar[j]=='?')){
-                i++;
-                j++;
+    int solve(int i,int j,string s,string p,vector<vector<int>>&dp){
+        if(i==0 && j==0){
+            return true;
+        }
+        if(i==0){
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*'){
+                    return false;
+                }
             }
-            else if(j<tar.size() && tar[j]=='*'){
-                startidx=j;
-                match=i;
-                j++;
-            }
-            else if(startidx!=-1){
-                match++;
-                i=match;
-                j=startidx+1;
-            }
-            else{
-                return false;
+            return true;
+        }
+        if(j==0){
+            return false;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i-1]==p[j-1] || p[j-1]=='?'){
+            return dp[i][j]=solve(i-1,j-1,s,p,dp);
+        }
+        else if(p[j-1]=='*'){
+            return dp[i][j]=solve(i-1,j,s,p,dp) || solve(i,j-1,s,p,dp);
+        }
+        return dp[i][j]=false;
+    }
+    bool isMatch(string s, string p) {
+        int l1=s.size();
+        int l2=p.size();
+        vector<vector<bool>>dp(l1+1,vector<bool>(l2+1,false));
+        // return solve(l1,l2,s,p,dp)==1?true:false;
+        dp[0][0]=true;
+        for(int j=1;j<=l2;j++){
+            if(p[j-1]=='*'){
+                dp[0][j]= dp[0][j-1];
             }
         }
-        while(j<tar.size() && tar[j]=='*'){
-            j++;
+        for(int i=1;i<=l1;i++){
+            dp[i][0]=false;
         }
-        return j==tar.size();
+        for(int i=1;i<=l1;i++){
+            for(int j=1;j<=l2;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                     dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                     dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                }
+                else dp[i][j]=false;
+            }
+        }
+        return dp[l1][l2];
     }
 };
