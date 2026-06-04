bef689a44f46c88812a537e705afae27f3bdb33a
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-04 18:05:03 +0530

    Time: 0 ms (100%), Space: 20 MB (81.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,6 +18,14 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
+        // greedy  optimal o(n) if stock price of tmr greater than today then buy stock today sell tmr
+        int res=0;
+        for(int i=1;i<n;i++){
+            if(prices[i]>=prices[i-1]){
+                res+=(prices[i]-prices[i-1]);
+            }
+        }
+        return res;
         // vector<vector<int>>dp(n+1,vector<int>(2,-1));
         // return solve(0,1,prices,n,dp);
         // dp[n][0]=dp[n][1]=0;
@@ -35,21 +43,21 @@ public:
         // }
         // return dp[0][1];
         // space optimization
-        vector<int>prev(2,0),cur(2,0);
-        prev[0]=prev[1]=0;
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                long profit=0;
-                if(buy){
-                    profit=max(-prices[i]+prev[0],prev[1]);
-                }
-                else{
-                    profit=max(prices[i]+prev[1],prev[0]);
-                }
-                cur[buy]=profit;
-            }
-            prev=cur;
-        }
-        return prev[1];
+        // vector<int>prev(2,0),cur(2,0);
+        // prev[0]=prev[1]=0;
+        // for(int i=n-1;i>=0;i--){
+        //     for(int buy=0;buy<=1;buy++){
+        //         long profit=0;
+        //         if(buy){
+        //             profit=max(-prices[i]+prev[0],prev[1]);
+        //         }
+        //         else{
+        //             profit=max(prices[i]+prev[1],prev[0]);
+        //         }
+        //         cur[buy]=profit;
+        //     }
+        //     prev=cur;
+        // }
+        // return prev[1];
     }
 };
