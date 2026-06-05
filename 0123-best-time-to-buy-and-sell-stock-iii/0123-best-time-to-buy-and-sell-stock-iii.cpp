bbec06cafb56389502d99f8086e212da4322762d
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-05 09:35:13 +0530

    Time: 68 ms (55.77%), Space: 79.2 MB (93.94%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -23,17 +23,48 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        // return solve(0,1,2,prices,n,dp);
-        // base case i==0 ,trade==0
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        // // return solve(0,1,2,prices,n,dp);
+        // // base case i==0 ,trade==0
+        // for(int buy=0;buy<2;buy++){
+        //     for(int trade=0;trade<3;trade++){
+        //         dp[n][buy][trade]=0;
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int buy=0;buy<2;buy++){
+        //         dp[i][buy][0]=0;
+        //     }
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     for(int buy=0;buy<2;buy++){
+        //         for(int trade=1;trade<3;trade++){
+        //             int profit=0;
+        //             if(buy){
+        //                 profit=max(-prices[i]+dp[i+1][0][trade],
+        //                 dp[i+1][1][trade]);
+        //             }
+        //             else{
+        //                 profit=max(prices[i]+dp[i+1][1][trade-1],
+        //                 dp[i+1][0][trade]);
+        //             }
+        //             dp[i][buy][trade]=profit;
+        //         }
+        //     }
+        // }
+        // return dp[0][1][2];
+        // space optimize
+
+        vector<vector<int>>prev(2,vector<int>(3,0)),cur(2,vector<int>(3,0));
         for(int buy=0;buy<2;buy++){
             for(int trade=0;trade<3;trade++){
-                dp[n][buy][trade]=0;
+               prev[buy][trade]=0;
             }
         }
         for(int i=0;i<n;i++){
             for(int buy=0;buy<2;buy++){
-                dp[i][buy][0]=0;
+                prev[buy][0]=0;
+                cur[buy][0]=0;
             }
         }
         for(int i=n-1;i>=0;i--){
@@ -41,17 +72,18 @@ public:
                 for(int trade=1;trade<3;trade++){
                     int profit=0;
                     if(buy){
-                        profit=max(-prices[i]+dp[i+1][0][trade],
-                        dp[i+1][1][trade]);
+                        profit=max(-prices[i]+prev[0][trade],
+                        prev[1][trade]);
                     }
                     else{
-                        profit=max(prices[i]+dp[i+1][1][trade-1],
-                        dp[i+1][0][trade]);
+                        profit=max(prices[i]+prev[1][trade-1],
+                        prev[0][trade]);
                     }
-                    dp[i][buy][trade]=profit;
+                    cur[buy][trade]=profit;
                 }
             }
+            prev=cur;
         }
-        return dp[0][1][2];
+        return prev[1][2];
     }
 };
