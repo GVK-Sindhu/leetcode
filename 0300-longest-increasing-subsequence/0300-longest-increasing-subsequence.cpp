20e23989adcf23decff2131f9dadfad007629f91
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-05 17:55:15 +0530

    Time: 275 ms (29.43%), Space: 296.6 MB (5.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -9,12 +9,26 @@ public:
         if(prv==-1 || nums[i]>nums[prv]){
             take=1+solve(i+1,i,nums,dp);
         }
-        return dp[i][prv+1
-        ]=max(take,nottake);
+        return dp[i][prv+1]=max(take,nottake);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // return solve(0,-1,nums,dp);
+        for(int prvi=0;prvi<n;prvi++){
+            dp[n][prvi]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int prvi=i-1;prvi>=-1;prvi--){
+                int nottake=dp[i+1][prvi+1];
+              int take=0;
+        
+            if(prvi==-1 || nums[i]>nums[prvi]){
+                take=1+dp[i+1][i+1];
+            }
+              dp[i][prvi+1]=max(take,nottake);
+            }
+        }
+        return dp[0][0];
     }
 };
