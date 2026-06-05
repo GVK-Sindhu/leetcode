efcee68df6cf90520c4d2293fc18f61da2504111
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-05 10:19:44 +0530

    Time: 0 ms (100%), Space: 17.6 MB (57.07%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -14,7 +14,31 @@ public:
     }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2*k,-1));
-        return solve(0,0,k,prices,dp);
+        vector<vector<int>>dp(n+1,vector<int>((2*k)+1,0));
+        // return solve(0,0,k,prices,dp);
+        // tabulation
+        for(int i=0;i<n;i++){
+            dp[i][(2*k)]=0;
+        }
+        for(int j=0;j<(2*k);j++){
+            dp[n][j]=0;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int trans=0;trans<(2*k);trans++){
+                int profit=0;
+
+                if(trans%2==0){
+                    profit=max( -prices[i]+dp[i+1][trans+1], dp[i+1][trans]);
+                }
+                else{
+                    profit=max( prices[i]+dp[i+1][trans+1], dp[i+1][trans]);
+                }
+                 dp[i][trans]=profit;
+            }
+        }
+        return dp[0][0];
+        // space optimization
+
     }
 };
