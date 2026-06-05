a460a20df0d39d38cd31cf550cd6ea6165e986d4
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-05 13:41:00 +0530

    Time: 572 ms (10.79%), Space: 296.5 MB (11.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+    int solve(int i,int prv,vector<int>& nums,vector<vector<int>>&dp){
+        if(i==nums.size()) return 0;
+        if(dp[i][prv+1]!=-1) return dp[i][prv+1];
+        int nottake=solve(i+1,prv,nums,dp);
+        int take=0;
+        
+        if(prv==-1 || nums[i]>nums[prv]){
+            take=1+solve(i+1,i,nums,dp);
+        }
+        return dp[i][prv+1
+        ]=max(take,nottake);
+    }
     int lengthOfLIS(vector<int>& nums) {
-       int n=nums.size();
-       vector<int>LIS;
-       LIS.push_back(nums[0]);
-       for(int i=1;i<n;i++){
-         if(LIS.back()<nums[i]){
-            LIS.push_back(nums[i]);
-         }
-         else{
-            auto it=lower_bound(LIS.begin(),LIS.end(),nums[i])-LIS.begin();
-            LIS[it]=nums[i];
-         }
-       }
-       return LIS.size();
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return solve(0,-1,nums,dp);
     }
 };
