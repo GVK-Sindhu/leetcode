6aba1cac2c108c11c98d6ba9f310c98e8a1998dc
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-05 11:41:20 +0530

    Time: 115 ms (33.96%), Space: 103.2 MB (49.6%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int solve(int i,int buy,vector<int>& prices,int fee,vector<vector<int>>&dp){
+        if(i==prices.size()){
+            return 0;
+        }
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
+        }
+        int profit=0;
+        if(buy){
+            profit=max( -prices[i]+solve(i+1,0,prices,fee,dp),solve(i+1,1,prices,fee,dp) );
+        }
+        else{
+             profit=max( prices[i]-fee+solve(i+1,1,prices,fee,dp),solve(i+1,0,prices,fee,dp) );
+        }
+        return dp[i][buy]=profit;
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        // return solve(0,1,prices,fee,dp);
+        // tabulation
+        for(int i=n-1;i>=0;i--){
+            dp[i][1]=max( -prices[i]+dp[i+1][0],dp[i+1][1] );
+            dp[i][0]=max( prices[i]-fee+dp[i+1][1],dp[i+1][0]);
+        }
+        return dp[0][1];
+        // space optimize
+
+    }
+};
