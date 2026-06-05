68d615a323a9c2b642dbfbbedbfd726b205ada4e
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-05 09:58:48 +0530

    Time: 28 ms (66.46%), Space: 79.2 MB (85.2%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -20,60 +20,47 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(5,0));
+        // vector<vector<int>>dp(n+1,vector<int>(5,0));
         // return solve(0,0,prices,n,dp);
         // base case i==n ,trans==4
-        dp[n][0]=dp[n][1]=dp[n][2]=dp[n][3]=0;
-        for(int i=0;i<n;i++){
-            dp[i][4]=0;
-        }
+        // dp[n][0]=dp[n][1]=dp[n][2]=dp[n][3]=0;
+        // for(int i=0;i<n;i++){
+        //     dp[i][4]=0;
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     for(int trans=0;trans<4;trans++){
+        //             int profit=0;
+        //             if(trans%2==0){
+        //                 profit=max(-prices[i]+dp[i+1][trans+1],
+        //                 dp[i+1][trans]);
+        //             }
+        //             else{
+        //                 profit=max(prices[i]+dp[i+1][trans+1],
+        //                 dp[i+1][trans]);
+        //             }
+        //             dp[i][trans]=profit;
+        //         }
+        // }
+        // return dp[0][0];
+        
+        //space optimize
+        vector<int>prev(5,0),cur(5,0);
         for(int i=n-1;i>=0;i--){
             for(int trans=0;trans<4;trans++){
                     int profit=0;
                     if(trans%2==0){
-                        profit=max(-prices[i]+dp[i+1][trans+1],
-                        dp[i+1][trans]);
+                        profit=max(-prices[i]+prev[trans+1],
+                        prev[trans]);
                     }
                     else{
-                        profit=max(prices[i]+dp[i+1][trans+1],
-                        dp[i+1][trans]);
+                        profit=max(prices[i]+prev[trans+1],
+                        prev[trans]);
                     }
-                    dp[i][trans]=profit;
+                    cur[trans]=profit;
                 }
+                prev=cur;
         }
-        return dp[0][0];
-        
-        // space optimize
+        return prev[0];
 
-        // vector<vector<int>>prev(2,vector<int>(3,0)),cur(2,vector<int>(3,0));
-        // for(int buy=0;buy<2;buy++){
-        //     for(int trade=0;trade<3;trade++){
-        //        prev[buy][trade]=0;
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int buy=0;buy<2;buy++){
-        //         prev[buy][0]=0;
-        //         cur[buy][0]=0;
-        //     }
-        // }
-        // for(int i=n-1;i>=0;i--){
-        //     for(int buy=0;buy<2;buy++){
-        //         for(int trade=1;trade<3;trade++){
-        //             int profit=0;
-        //             if(buy){
-        //                 profit=max(-prices[i]+prev[0][trade],
-        //                 prev[1][trade]);
-        //             }
-        //             else{
-        //                 profit=max(prices[i]+prev[1][trade-1],
-        //                 prev[0][trade]);
-        //             }
-        //             cur[buy][trade]=profit;
-        //         }
-        //     }
-        //     prev=cur;
-        // }
-        // return prev[1][2];
     }
 };
