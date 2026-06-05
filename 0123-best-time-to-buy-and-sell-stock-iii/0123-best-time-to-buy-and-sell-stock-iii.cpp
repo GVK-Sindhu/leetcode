dfa9e28b836fa45b4056c8e185903d3912241a1b
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-05 09:47:59 +0530

    Time: 185 ms (48.79%), Space: 138.7 MB (51.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int solve(int i,int buy,int trade,vector<int>& prices,int n,vector<vector<vector<int>>>&dp){
-        if(i==n){
+    int solve(int i,int trans,vector<int>& prices,int n,vector<vector<int>>&dp){
+        if(i==n || trans==4){
             return 0;
         }
-        if(trade==0){
-            return 0;
-        }
-        if(dp[i][buy][trade]!=-1){
-            return dp[i][buy][trade];
+        if(dp[i][trans]!=-1){
+            return dp[i][trans];
         }
         int profit=0;
-        if(buy){
-            profit=max( -prices[i]+solve(i+1,0,trade,prices,n,dp),
-            solve(i+1,1,trade,prices,n,dp));
+        if(trans%2==0){
+            profit=max( -prices[i]+solve(i+1,trans+1,prices,n,dp),
+            solve(i+1,trans,prices,n,dp));
         }
         else{
-            profit=max( prices[i]+solve(i+1,1,trade-1,prices,n,dp),
-            solve(i+1,0,trade,prices,n,dp));
+            profit=max( prices[i]+solve(i+1,trans+1,prices,n,dp),
+            solve(i+1,trans,prices,n,dp));
         }
-        return dp[i][buy][trade]=profit;
+        return dp[i][trans]=profit;
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        // // return solve(0,1,2,prices,n,dp);
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return solve(0,0,prices,n,dp);
         // // base case i==0 ,trade==0
         // for(int buy=0;buy<2;buy++){
         //     for(int trade=0;trade<3;trade++){
@@ -53,37 +50,38 @@ public:
         //     }
         // }
         // return dp[0][1][2];
+        
         // space optimize
 
-        vector<vector<int>>prev(2,vector<int>(3,0)),cur(2,vector<int>(3,0));
-        for(int buy=0;buy<2;buy++){
-            for(int trade=0;trade<3;trade++){
-               prev[buy][trade]=0;
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int buy=0;buy<2;buy++){
-                prev[buy][0]=0;
-                cur[buy][0]=0;
-            }
-        }
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<2;buy++){
-                for(int trade=1;trade<3;trade++){
-                    int profit=0;
-                    if(buy){
-                        profit=max(-prices[i]+prev[0][trade],
-                        prev[1][trade]);
-                    }
-                    else{
-                        profit=max(prices[i]+prev[1][trade-1],
-                        prev[0][trade]);
-                    }
-                    cur[buy][trade]=profit;
-                }
-            }
-            prev=cur;
-        }
-        return prev[1][2];
+        // vector<vector<int>>prev(2,vector<int>(3,0)),cur(2,vector<int>(3,0));
+        // for(int buy=0;buy<2;buy++){
+        //     for(int trade=0;trade<3;trade++){
+        //        prev[buy][trade]=0;
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int buy=0;buy<2;buy++){
+        //         prev[buy][0]=0;
+        //         cur[buy][0]=0;
+        //     }
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     for(int buy=0;buy<2;buy++){
+        //         for(int trade=1;trade<3;trade++){
+        //             int profit=0;
+        //             if(buy){
+        //                 profit=max(-prices[i]+prev[0][trade],
+        //                 prev[1][trade]);
+        //             }
+        //             else{
+        //                 profit=max(prices[i]+prev[1][trade-1],
+        //                 prev[0][trade]);
+        //             }
+        //             cur[buy][trade]=profit;
+        //         }
+        //     }
+        //     prev=cur;
+        // }
+        // return prev[1][2];
     }
 };
