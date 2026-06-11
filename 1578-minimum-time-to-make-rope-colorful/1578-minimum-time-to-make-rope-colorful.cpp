85fe0fb3c045c0de55060edd28fb61a0470e4712
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-11 14:39:44 +0530

    Time: 12 ms (14.12%), Space: 99.9 MB (64.7%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int res=0;
-        for(int i=0;i<colors.size()-1;i++){
-            if(colors[i]==colors[i+1]){
-                res+=min(neededTime[i],neededTime[i+1]);
-                neededTime[i]=max(neededTime[i],neededTime[i+1]);
+         int n = colors.size();
+        int res = 0;
+
+        int i = 0;
+        while (i < n) {
+            int sum = neededTime[i];
+            int mx = neededTime[i];
+
+            int j = i + 1;
+
+            while (j < n && colors[j] == colors[i]) {
+                sum += neededTime[j];
+                mx = max(mx, neededTime[j]);
+                j++;
             }
+
+            res += (sum - mx);
+            i = j;
         }
+
         return res;
     }
 };
+
