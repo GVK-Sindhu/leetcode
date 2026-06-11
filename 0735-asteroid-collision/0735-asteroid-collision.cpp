d33a8d941119c885c5c210899a192369345f6431
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-11 12:16:19 +0530

    Time: 0 ms (100%), Space: 22 MB (67.97%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& a) {
-     stack<int>st;
-     int n=a.size();
-     for(int i=0;i<n;i++){
-       bool alive=true;
-       while(!st.empty() && st.top()>0 && a[i]<0 && alive){ //we check only for -ve ele and also where stack top has +ve ele
-        if(st.top()<(-a[i])){ //if it is -10,5 the 5 will be destroyed later -10 can be pushed to stack
+        stack<int>st;
+        // for(int i=0;i<a.size();i++){
+        //    bool alive=true;
+        //    while(!st.empty() && (st.top()>0 && a[i]<0)){
+        //       int f1=st.top();
+        //       int f2=abs(a[i]);
+        //       if(f1==f2){
+        //         st.pop();
+        //       }
+        //       else if(f1<f2){
+
+        //       }
+        //       else{
+
+        //       }
+        //    }
+        // }
+        
+for(int i=0;i<a.size();i++){
+    bool alive = true;
+
+    while(!st.empty() && st.top() > 0 && a[i] < 0){
+
+        int a1 = abs(st.top());
+        int b  = abs(a[i]);
+
+        if(a1 < b){
             st.pop();
         }
-        else if(st.top()==(-a[i])){//if it is -10,10 both will destroy each other and there is no chance of adding -10 into stack so alive=false
+        else if(a1 == b){
             st.pop();
-            alive=false;
+            alive = false;
+            break;
         }
-        else{ //if 10,-5 stack top is > -5 so -5 will not be included in stack so alive=false
-           alive=false;
+        else{
+            alive = false;
+            break;
         }
-       }
-       if(alive){
-        st.push(a[i]);
-       }
     }
-     vector<int>res;
-     while(!st.empty()){
-        res.push_back(st.top());
-        st.pop();
-     }
-     reverse(res.begin(),res.end());
-     return res;
-   
+
+    if(alive)
+        st.push(a[i]);
+}
+        vector<int>res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
