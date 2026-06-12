a386f08b12ae5bc939c923fea781248537dce097
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-12 15:59:41 +0530

    Time: 3 ms (28.38%), Space: 22 MB (58.79%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,48 +2,28 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& a) {
         stack<int>st;
-        // for(int i=0;i<a.size();i++){
-        //    bool alive=true;
-        //    while(!st.empty() && (st.top()>0 && a[i]<0)){
-        //       int f1=st.top();
-        //       int f2=abs(a[i]);
-        //       if(f1==f2){
-        //         st.pop();
-        //       }
-        //       else if(f1<f2){
+        for(int i=0;i<a.size();i++){
+            bool alive = true;
+            while(!st.empty() && st.top() > 0 && a[i] < 0){
+                int a1 = abs(st.top());
+                int b  = abs(a[i]);
+                if(a1 < b){
+                    st.pop();
+                }
+                else if(a1 == b){
+                    st.pop();
+                    alive = false;
+                    break;
+                }
+                else{
+                    alive = false;
+                    break;
+                }
+            }
 
-        //       }
-        //       else{
-
-        //       }
-        //    }
-        // }
-        
-for(int i=0;i<a.size();i++){
-    bool alive = true;
-
-    while(!st.empty() && st.top() > 0 && a[i] < 0){
-
-        int a1 = abs(st.top());
-        int b  = abs(a[i]);
-
-        if(a1 < b){
-            st.pop();
+            if(alive)
+                st.push(a[i]);
         }
-        else if(a1 == b){
-            st.pop();
-            alive = false;
-            break;
-        }
-        else{
-            alive = false;
-            break;
-        }
-    }
-
-    if(alive)
-        st.push(a[i]);
-}
         vector<int>res;
         while(!st.empty()){
             res.push_back(st.top());
