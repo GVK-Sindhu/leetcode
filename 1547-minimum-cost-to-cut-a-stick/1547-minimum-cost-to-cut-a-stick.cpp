42ff33ccf0ac4219efe0fda47e4dab4d1bb1ee09
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-16 13:51:33 +0530

    Time: 19 ms (80.1%), Space: 14 MB (24.93%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<int>& cuts,vector<vector<int>>&dp){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int mini=INT_MAX;
+        for(int idx=i;idx<=j;idx++){
+            int cost=cuts[j+1]-cuts[i-1]+solve(i,idx-1,cuts,dp)+solve(idx+1,j,cuts,dp);
+            mini=min(mini,cost);
+        }
+        return dp[i][j]=mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(),cuts.end());
+        int len=cuts.size()-2;
+        vector<vector<int>>dp(len+2,vector<int>(len+2,0));
+        // return solve(1,len,cuts,dp);
+        for(int i=len;i>=1;i--){
+            for(int j=1;j<=len;j++){
+                if(i>j) continue;
+                int mini=INT_MAX;
+                for(int idx=i;idx<=j;idx++){
+                    int cost=cuts[j+1]-cuts[i-1]+dp[i][idx-1]+dp[idx+1][j];
+                    mini=min(mini,cost);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][len];
+    }
+};
