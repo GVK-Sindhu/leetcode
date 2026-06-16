56ef7937f49c14b313764822414322a01a6d3fb3
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-16 09:16:00 +0530

    Time: 5 ms (45.83%), Space: 39.6 MB (87.5%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*' && !res.empty()){
+                res.pop_back();
+            }
+            else if(s[i]=='#'){
+               res+=res;
+            }
+            else if(s[i]=='%'){
+                reverse(res.begin(),res.end());
+            }
+            else if(s[i]<='z' && s[i]>='a'){
+                res+=s[i];
+            }
+        }
+        return res;
+    }
+};
