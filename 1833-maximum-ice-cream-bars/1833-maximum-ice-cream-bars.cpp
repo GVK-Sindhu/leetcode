2d398f997293c1e567d6c6b7ebbc8d430f2beb67
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-06-21 14:48:30 +0530

    Time: 35 ms (49.96%), Space: 80.2 MB (89.61%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -3,10 +3,14 @@ public:
     int maxIceCream(vector<int>& costs, int coins) {
         sort(costs.begin(),costs.end());
         int s=0,c=0;
-        for(int i=0;i<costs.size();i++){
-            s+=costs[i];
-            if(s>coins) return c;
-            else c++;
+        for(int it:costs){
+            if((s+it)<=coins){
+                s+=it;
+                c++;
+            }
+            else{
+                return c;
+            }
         }
         return c;
     }
