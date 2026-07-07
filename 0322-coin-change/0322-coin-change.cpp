58fd5a012ccce0b575565993b09692345c85c085
GANTADA VENKATA KAVYA SINDHU <23A91A0595@aec.edu.in>
2026-07-07 21:54:29 +0530

    Time: 53 ms (19.88%), Space: 49.5 MB (25.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,7 +4,7 @@ public:
         if(tar==0){
             return 0;
         }
-        if(i==0){
+        if(i==nums.size()-1){
             if(tar%nums[i]==0){
                 return tar/nums[i];
             }
@@ -15,7 +15,7 @@ public:
         if(dp[i][tar]!=-1){
             return dp[i][tar];
         }
-        int nottake=solve(i-1,nums,tar,dp);
+        int nottake=solve(i+1,nums,tar,dp);
         int take=1e9;
         if(nums[i]<=tar) take=1+solve(i,nums,tar-nums[i],dp);
         return dp[i][tar]=min(take,nottake);
@@ -23,7 +23,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        // return solve(n-1,coins,amount,dp)>=1e9?-1:solve(n-1,coins,amount,dp);
+        return solve(0,coins,amount,dp)>=1e9?-1:solve(0,coins,amount,dp);
         // for(int i=1;i<=amount;i++){
         //     if(i%coins[0]==0){
         //         dp[0][i]=i/coins[0];
@@ -46,26 +46,26 @@ public:
         // return dp[n-1][amount]>=1e9?-1:dp[n-1][amount];
 
         // space optimization
-        vector<int>prev(amount+1,-1),cur(amount+1,-1);
-        for(int i=1;i<=amount;i++){
-            if(i%coins[0]==0){
-                prev[i]=i/coins[0];
-            }
-            else{
-                prev[i]=1e9;
-            }
-        }
-        prev[0]=0;
-        cur[0]=0;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=amount;j++){
-                int nottake=prev[j];
-                int take=1e9;
-                if(coins[i]<=j) take=1+cur[j-coins[i]];
-                cur[j]=min(take,nottake);
-            }
-            prev=cur;
-        }
-        return prev[amount]>=1e9?-1:prev[amount];
+        // vector<int>prev(amount+1,-1),cur(amount+1,-1);
+        // for(int i=1;i<=amount;i++){
+        //     if(i%coins[0]==0){
+        //         prev[i]=i/coins[0];
+        //     }
+        //     else{
+        //         prev[i]=1e9;
+        //     }
+        // }
+        // prev[0]=0;
+        // cur[0]=0;
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<=amount;j++){
+        //         int nottake=prev[j];
+        //         int take=1e9;
+        //         if(coins[i]<=j) take=1+cur[j-coins[i]];
+        //         cur[j]=min(take,nottake);
+        //     }
+        //     prev=cur;
+        // }
+        // return prev[amount]>=1e9?-1:prev[amount];
     }
 };
